61507cae22f9f0c77e7a781ba02e34c510c355f9
Glitch (ztube) <none>
2020-12-23 00:53:24 +0000

    Making some changes in the server

--- index.js
+++ index.js
@@ -4,17 +4,36 @@ const ytdl = require("ytdl-core")
 const app = express()
 
 app.use(cors())
-
-app.listen(4000, () => {
-    console.log("Server running at port 4000")
+app.set("views", __dirname, "/views")
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/index.html")
+})
+app.get('/style.css', function(req, res) {
+  res.sendFile(__dirname + "css/background.css")
+})
+app.get('/style.css', function(req, res) {
+  res.sendFile(__dirname + "css/button.css")
+})
+app.get('/style.css', function(req, res) {
+  res.sendFile(__dirname + "css/text.css")
+})
+app.get('/style.css', function(req, res) {
+  res.sendFile(__dirname + "css/video.css")
 })
 
-app.get("/download", (req,res) => {
+app.get("/download", function(req,res){
     var URL = req.query.URL
-    
     res.header("Content-Disposition", 'attachment; filename="video.mp4"')
 
     ytdl(URL, {
         format: "mp4"
         }).pipe(res)
+})
+
+app.get("/*", function(req,res){
+  res.redirect("/")
+})
+
+app.listen(4000, function(){
+    console.log("Server running at port 4000")
 })
